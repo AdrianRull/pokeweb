7d1b3666e0e30e08643c5d951d90f9651c20a2aa
AdrianRull <151536090+AdrianRull@users.noreply.github.com>
2023-11-21 17:47:29 +0100

    pokeweb

---
+++ Workspace/javasc/index.js
@@ -0,0 +1,17 @@
+console.log("hola");
+var miVariable = 24
+
+miVariable = 12
+
+const miConstante = 3
+
+var op1 = 2
+var op2= 3
+var resultado = op1 + op2console.log("resultado vale: " + resultado)
+
+let miNumero = 6
+if (miNumero == 6)(
+    console.log('Si. Mi numero vale 6')
+)
+var producto = 5 * 4;
+console.log(producto)
